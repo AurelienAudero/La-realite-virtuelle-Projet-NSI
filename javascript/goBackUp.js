9d3035aff8275dd5ab813ab0bd9313eebaddcd6f
AurelienAudero <64486562+AurelienAudero@users.noreply.github.com>
2023-04-10 17:38:16 +0200

    Ajout d'un bouton pour revenir en haut de la page

---
+++ javascript/goBackUp.js
@@ -0,0 +1,25 @@
+// Récupère l'élément #home-up-arrow
+const homeUpArrow = document.querySelector('#home-up-arrow');
+
+// Récupère l'élément footer
+const footer = document.querySelector('.footer-container');
+
+// Récupère la position verticale du footer par rapport au haut de la page
+const footerPosition = footer.getBoundingClientRect().top + window.scrollY;
+
+// Ajoute un écouteur d'événement qui se déclenche à chaque fois que l'utilisateur scroll
+window.addEventListener('scroll', () => {
+  // Récupère la position verticale actuelle de l'utilisateur
+  const scrollPosition = window.scrollY + window.innerHeight;
+
+  // Vérifie si l'utilisateur a atteint le footer
+  if (scrollPosition >= footerPosition) {
+    // Si oui, change le positionnement de l'élément #home-up-arrow en "absolute"
+    homeUpArrow.style.position = `unset`;
+  } else {
+    // Sinon, change le positionnement de l'élément #home-up-arrow en "fixed"
+    homeUpArrow.style.position = 'fixed';
+    homeUpArrow.style.bottom = '15px';
+    homeUpArrow.style.right = '0px';
+  }
+});
